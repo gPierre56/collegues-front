03fc545da68f4ceb83291cc047cb0257499d2075
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-23 17:28:48 +0200

    changements html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,7 @@ import {CollegueMock} from "./mock/collegue.mock";
 
 @Component({
   selector: 'app-root',
-  template : `
-      <div class = "row">
-        <div class="col-3">
-        <app-recherche-collegue-par-nom></app-recherche-collegue-par-nom>
-        </div>
-        
-        <div class="col-9">
-        <app-collegue [col] = "col"> </app-collegue>
-      </div>
-      
-        </div>
-      
-  
-  `,
+  templateUrl : './app.component.html',
   styles: []
 })
 
