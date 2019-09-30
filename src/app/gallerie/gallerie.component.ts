ff287e4a8e0c0e3f863b3a6d232fc03e4db85e61
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-30 16:22:58 +0200

    modifications navigation

--- src/app/gallerie/gallerie.component.ts
+++ src/app/gallerie/gallerie.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PhotoService} from "../services/photo.service";
 import {PhotoDto} from "../dto/photoDto";
 import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-gallerie',
@@ -13,12 +14,16 @@ export class GallerieComponent implements OnInit {
   actionSub: Subscription;
   @Input() listePhotos: Array<PhotoDto>;
 
-  constructor(private _photoService: PhotoService) { }
+  constructor(private _photoService: PhotoService, private router: Router) { }
 
   ngOnInit() {
     this.actionSub = this._photoService.recupererToutesLesPhotos().subscribe((liste) => {
       this.listePhotos = liste;
     })
+  }
+
+  afficherDetailsCollegue(matricule: string) {
+    this.router.navigate(['gallerie/'.concat(matricule)])
   }
 
 
